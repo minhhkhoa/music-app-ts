d0b69389a79b204b862390f3919c46005a5dd6ea
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-18 15:39:38 +0700

    2.5. Lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response} from "express"
 import dotenv from "dotenv"
 import * as database from "./config/database"
 
+import Topic from "./models/topic.model"
+
 dotenv.config()
 database.connect() //-goi ham connect
 
@@ -13,8 +15,14 @@ app.set("views", "./views") //-xet thu muc views
 app.set("view engine", "pug") //- V s/d: pug
 //-end pug view
 
-app.get("/topics", (req: Request, res: Response) => {
-  res.render("client/pages/topics/index")
+app.get("/topics", async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  })
+  console.log(topics)
+  res.render("client/pages/topics/index",{
+
+  })
 })
 
 app.listen(port, () => {
