3ac8d27d361ca5d1b95a170bb7260e7a47812690
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-25 08:33:15 +0700

    Trang Quản lý chủ đề (bên admin)

---
+++ controllers/admin/topic.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+
+//[get] /admin/topic
+export const index = async (req: Request, res: Response) => {
+  const topics = await Topic.find({
+    deleted: false
+  })
+  
+  
+  res.render("admin/pages/topics/index", {
+    pageTitle: "Quản lý chủ đề",
+    topics: topics
+  })
+}
--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
 import { dashboardRoutes } from "./dashboard.route";
 import { systemConfig } from "../../config/config";
+import { topicRoutes } from "./topic.route";
 
 
 const adminRoutes = (app: Express): void => {
@@ -8,6 +9,8 @@ const adminRoutes = (app: Express): void => {
   const PATH_ADMIN = `/${systemConfig.prefixAdmin}`
 
   app.use(`${PATH_ADMIN}/dashboard`, dashboardRoutes)
+
+  app.use(`${PATH_ADMIN}/topics`, topicRoutes)
 
 }
 
