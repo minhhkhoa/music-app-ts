09ff659a2a6caa03fc29bfb047cc0cfd1e0f5ae3
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-25 17:20:51 +0700

    1.2. Tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -13,3 +13,21 @@ if (uploadImage) {
   })
 }
 //-End Upload preview image
+
+//-Start Upload preview audio
+const uploadAudio = document.querySelector("[upload-audio]")
+if (uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]")
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]")
+  const source = uploadAudio.querySelector("source")
+
+  uploadAudioInput.addEventListener("change", (e) => {
+    const file = e.target.files[0]
+    if (file) {
+      //-gan scr bang dg dan toi file anh
+      source.src = URL.createObjectURL(file)
+      uploadAudioPlay.load() //-load file đứng từ thẻ cha load
+    }
+  })
+}
+//-End Upload preview audio
