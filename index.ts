b8eb78f14e568a411e43d5d927827a12cade9a1d
minhhkhoa <khoanguyen.1881@gmail.com>
2024-10-25 08:06:22 +0700

    Layout chung bên Admin

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express, { Express } from "express"
 import dotenv from "dotenv"
 import * as database from "./config/database"
+import adminRoutes from "./routes/admin/index.route"
 import clientRoutes from "./routes/client/index.route"
+import { systemConfig } from "./config/config"
 
 dotenv.config()
 database.connect() //-goi ham connect
@@ -16,9 +18,17 @@ app.set("views", "./views") //-xet thu muc views
 app.set("view engine", "pug") //- V s/d: pug
 //-end pug view
 
+//-App lacal variables(tạo biến toàn cục để bất kể ở file bug nào cx dùng dc prefixAdmin)
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
+//-admin route
+adminRoutes(app)
+//- end admin route
+
 //-client route
 clientRoutes(app)
 //-end client route
+
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
